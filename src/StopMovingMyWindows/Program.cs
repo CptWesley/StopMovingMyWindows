320e430d0a328bbc7c9f14035857c41284b807ed
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2020-12-20 11:08:39 +0100

    Add basic project

---
+++ src/StopMovingMyWindows/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StopMovingMyWindows
+{
+    /// <summary>
+    /// The entry class of the application.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
